70a9309decd718cb09acb04bed7752da7ee658d1
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-05-10 23:41:06 +0500

    some error while previous push

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -15,49 +15,25 @@ class TalentedStudentsService extends BaseService{
         try{
             const page = req.query.page as unknown as number || 1
             const lang = req.params.lang as Lang["types"]
-            const data = await Talenteds.aggregate
-            metadata: [ { $count: "total" }, { $addFields: { page: page }},{$addFields: {limit:teacherListLimit}} ],
-            teachers: [ 
-                { $skip: (page-1)*teacherListLimit }, 
-                { $limit: teacherListLimit },{$unset:["__v"]},
-                // { $lookup:{
-                //     from:"departments",
-                //     localField:"departmentId",
-                //     pipeline:[{$project:{name:`$name_${lang}`,desc:`$desc_${lang}`,address:`$address_${lang}`,dean:"$dean"}}],
-                //     foreignField:"_id",
-                //     as:"department"
-                // }},
-                // {$unwind:{path:"$department",preserveNullAndEmptyArrays:true}}
-            ]
-        } },
-    {$unwind:{path:"$metadata",preserveNullAndEmptyArrays:true}}
-])
-return ResponseService.responseOK("",data)
-} catch (e) {
-console.log(e);
-
-return ResponseService.internalServerError(e)
-}
-}
-
-
-async getById(req:Request){
-try{
-const id = req.params.id
-const data = await Teachers.findById(new ObjectId(id))
-    .select("-__v")
-    .populate("departmentId","name -_id")
-if(!data) return ResponseService.notFound()
-return ResponseService.responseWithData(data)
-
-} catch (e) {
-return ResponseService.internalServerError(e)
-}
-}
-
-
-}
-export default TeacherService
+            const data = await Talenteds.aggregate([
+                { '$facet': {
+                    metadata: [ { $count: "total" }, { $addFields: { page: page }},{$addFields: {limit:talentedStudentListLimit}} ],
+                    data: [ 
+                        { $skip: (page-1)*talentedStudentListLimit },
+                        { $limit: talentedStudentListLimit },
+                        { $lookup:{
+                            from:"majors",
+                            localField:"majorId",
+                            foreignField:"_id",
+                            pipeline:[{$project:{name:`$name_${lang}`}}],
+                            as:"major"
+                        }},
+                        {$unwind:{path:"$major",preserveNullAndEmptyArrays:true}},
+                        { $unset:["__v","majorId"]},
+                    ]
+                } },
+                {$unwind:"$metadata"}
+            ])
             return ResponseService.responseWithData(data)
         } catch(e){
             return ResponseService.internalServerError(e)
