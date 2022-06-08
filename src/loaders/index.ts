b1c1df0760b77dac561695c2663d4f2f6d31c877
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-06-08 15:33:43 +0500

    some updates in loaders

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,4 +1,4 @@
-import express, {Application, Request, Response} from 'express'
+import express, {Application} from 'express'
 import * as dotenv from "dotenv"
 import notFound from "../middlewares/notFound";
 import connectDbAndRedis from "../middlewares/connectDbAndRedis";
